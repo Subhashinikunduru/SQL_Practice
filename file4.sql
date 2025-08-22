668c16b4164dc1fc7a35bda90a6691690f0cdf49
Subhashini <subhashinikunduru99@gmail.com>
2025-08-22 12:10:40 +0530

    ain'22-08-2025

--- file4.sql
+++ file4.sql
@@ -1,2 +1,6 @@
 use batch61;
+#second highestsalary
 select * from (select *,dense_rank() over(partition by deptno order by sal desc) as rnk from emp)  as emp2 where rnk=2;
+#update salary
+call Update_Sal(7369,10);#800---->880sal incre
+select*from emp;
