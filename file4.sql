444e8abda55adf6a12ab282d0095124c003ee133
Subhashini <subhashinikunduru99@gmail.com>
2025-09-07 21:13:39 +0530

    ain'07-09-2025

--- file4.sql
+++ file4.sql
@@ -17,4 +17,14 @@ drop trigger update_hire_date;
 create trigger update_hire_date before insert on emp 
 for each row set new.hiredate = curdate();
 
+select e.ename,e.sal,d.dname,d.loc,s.grade from (select deptno,ename,sal,hiredate) emp 
+join dept d on e.deptno=d.deptno join salgrade s on e.sal between s.losal and s.hisal where e.rn=1;
+
+
+select e.ename,e.sal,d.dname,d.loc,s.grade from emp e inner join dept d on e.deptno=d.deptno   join grade ;
+
+
+
+
+
 
